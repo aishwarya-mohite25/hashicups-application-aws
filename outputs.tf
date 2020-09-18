a4e0d5b205e79a204ef9849314f6caeb20efbb0d
Troy Fluegge <troy@hashicorp.com>
2020-09-17 23:17:56 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 output "output" {
   value = <<README
 
-frontend: ssh -i awskey.pem ec2-user@${module.frontend.public_ip}
-public-api: ssh -i awskey.pem ec2-user@${module.public_api.public_ip}
-product-api: ssh -i awskey.pem ec2-user@${module.product_api.public_ip}
-postgres: ssh -i awskey.pem ec2-user@${module.postgres.public_ip}
+frontend: ssh -i ~/.ssh/awskey.pem ec2-user@${module.frontend.public_ip}
+public-api: ssh -i ~/.ssh/awskey.pem ec2-user@${module.public_api.public_ip}
+product-api: ssh -i ~/.ssh/awskey.pem ec2-user@${module.product_api.public_ip}
+postgres: ssh -i ~/.ssh/awskey.pem ec2-user@${module.postgres.public_ip}
 
 Takes a while for docker:
 http://${module.frontend.public_ip}
